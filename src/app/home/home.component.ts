1f9b0fc3c234e5a937c0f897c14cb31db654a153
Tharindu <Dalugodatp@gmail.com>
2024-09-16 20:20:38 +0530

    Added popular movies

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,11 +8,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   trendingMovies: any;
+  theatreMovies: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getTrendingMovies();
+    this.getTheaterMovies();
   }
 
   getTrendingMovies() {
@@ -20,7 +22,14 @@ export class HomeComponent implements OnInit {
       .get('http://localhost:4200/assets/data/trending-movies.json')
       .subscribe((movies) => {
         this.trendingMovies = movies;
-        console.log(this.trendingMovies);
+      });
+  }
+
+  getTheaterMovies() {
+    this.http
+      .get('http://localhost:4200/assets/data/theatre-movies.json')
+      .subscribe((movies) => {
+        this.theatreMovies = movies;
       });
   }
 }
